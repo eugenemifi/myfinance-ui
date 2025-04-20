bdc791d32ba5c18be2345031b2d171a6d43eb4c6
Roman <astek14@mail.ru>
2025-04-20 19:04:13 +0300

    PROZY-3: removed the mock data display if the user isn't logged in

--- src/components/transactions.component.js
+++ src/components/transactions.component.js
@@ -1,10 +1,11 @@
 import {useEffect, useState} from "react";
 import DataFromMock from "../utils/util";
-import TransactionService from "../services/transaction.service";
+import AuthService from "../services/auth.service";
 
 function ListTransactions() {
 
     const [transactions, setTransactions] = useState(DataFromMock.getTransactions);
+    const [currentUser] = useState(AuthService.getCurrentUser);
     useEffect(() => {
         //TransactionService.getTransactions().then((response) => setTransactions(response)).catch((error) => {console.log(error)})
     });
@@ -13,34 +14,43 @@ function ListTransactions() {
         <>
             <div> List Transactions Component</div>
             <div> Data from Mock</div>
-            <div>
-                <h1 className = "text-center"> Transaction List</h1>
-                <table className = "table table-striped">
-                    <thead>
-                    <tr>
+            {currentUser ? (
+                    <div>
+                        <h1 className="text-center"> Transaction List</h1>
+                        <table className="table table-striped">
+                            <thead>
+                            <tr>
 
-                        <td> Transaction Id</td>
-                        <td> Transaction amount</td>
-                        <td> Transaction date</td>
-                        <td> Transaction category</td>
-                        <td> Transaction description</td>
-                    </tr>
+                                <td> Transaction Id</td>
+                                <td> Transaction amount</td>
+                                <td> Transaction date</td>
+                                <td> Transaction category</td>
+                                <td> Transaction description</td>
+                            </tr>
 
-                    </thead>
-                    <tbody>
+                            </thead>
+                            <tbody>
 
-                    {transactions.map(transaction =>
-                        <tr key = {transaction.id}>
-                            <td> {transaction.id}</td>
-                            <td> {transaction.amount}</td>
-                            <td> {transaction.date}</td>
-                            <td> {transaction.category}</td>
-                            <td> {transaction.description}</td>
-                        </tr>
-                    )}
-                    </tbody>
-                </table>
-            </div>
+                            {transactions.map(transaction =>
+                                <tr key={transaction.id}>
+                                    <td> {transaction.id}</td>
+                                    <td> {transaction.amount}</td>
+                                    <td> {transaction.date}</td>
+                                    <td> {transaction.category}</td>
+                                    <td> {transaction.description}</td>
+                                </tr>
+                            )}
+                            </tbody>
+                        </table>
+                    </div>
+                )
+                :
+                (
+                    <div>
+                        <strong> Вы не авторизованы! </strong>
+                    </div>
+                )}
+
         </>
     );
 }
--- src/components/users.components.js
+++ src/components/users.components.js
@@ -1,10 +1,12 @@
 import {useEffect, useState} from "react";
 import DataFromMock from "../utils/util"
 import {Link} from "react-router";
-import UserService from "../services/user.service";
+import AuthService from "../services/auth.service";
+
 function ListUsers() {
 
     const [users, setUsers] = useState(DataFromMock.getUsers);
+    const [currentUser] = useState(AuthService.getCurrentUser);
     useEffect(() => {
         //UserService.getUsers().then((response) => setUsers(response)).catch((error) => {console.log(error)})
     });
@@ -13,27 +15,29 @@ function ListUsers() {
         <>
             <div> List Users Component</div>
             <div> Data from Mock</div>
-            <div>
-                <h1 className = "text-center"> Users List</h1>
-                <table className = "table table-striped">
-                    <thead>
-                    <tr>
 
-                        <td> User Id</td>
-                        <td> User Login</td>
-                        <td> User First Name</td>
-                        <td> User Last Name</td>
-                        <td> User Email Id</td>
-                        <td> User Role</td>
-                        <td> User Create At</td>
-                        <td> User Update At</td>
-                    </tr>
 
-                    </thead>
-                    <tbody>
+            {currentUser ? (
+                    <div>
+                        <h1 className="text-center"> Users List</h1>
+                        <table className="table table-striped">
+                            <thead>
+                            <tr>
 
-                    {users.map(user =>
-                                <tr key = {user.id}>
+                                <td> User Id</td>
+                                <td> User Login</td>
+                                <td> User First Name</td>
+                                <td> User Last Name</td>
+                                <td> User Email Id</td>
+                                <td> User Role</td>
+                                <td> User Create At</td>
+                                <td> User Update At</td>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                            {users.map(user =>
+                                <tr key={user.id}>
                                     <td> {user.id}</td>
                                     <td> {user.login}</td>
                                     <td> {user.firstName}</td>
@@ -45,10 +49,16 @@ function ListUsers() {
                                     <td>{<Link to={`${user.id}`} className="btn btn-info">Редактировать</Link>}</td>
                                     <td>{<Link to={`/users`}>Удалить</Link>}</td>
                                 </tr>
-                    )}
-                    </tbody>
-                </table>
-            </div>
+                            )}
+                            </tbody>
+                        </table>
+                    </div>
+                ) :
+                (
+                    <div>
+                        <strong> Вы не авторизованы! </strong>
+                    </div>
+                )}
         </>
     );
 }
